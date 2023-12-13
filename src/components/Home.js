626a71640c8f3d67f40f72722422f7e915cdfce7
Swapnil1107 <shindes12121@gmail.com>
2023-12-13 22:41:26 +0530

    added heading branch 2 in home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 function Home() {
   return (
     <>
-      <h1>This is home page for counter</h1>
+      <h1>This is home page for counter </h1>
+      <h1> Branch 2 code  </h1>
     </>
   );
 }
